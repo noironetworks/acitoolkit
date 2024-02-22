99d7ccf17a25c35f0d937593e80cb4f50f0209a6
Noiro <noiro@os-bm-2-extrtr.noiro.lab>
2024-02-22 16:01:15 -0500

    Revert "Add support for Jammy series"
    This reverts commit 0ea604118db15c76aa770de999096c51a531942d.

--- acitoolkit/aciConcreteLib.py
+++ acitoolkit/aciConcreteLib.py
@@ -1102,7 +1102,7 @@ class ConcreteSVI(CommonConcreteObject):
         self.attr['vlan_id'] = str(attr['vlanId'])
         self.attr['vlan_type'] = str(attr['vlanT'])
         self.attr['dn'] = str(attr['dn'])
-        if self.attr['name'] == '':
+        if self.attr['name'] is '':
             self.name = self.attr['id']
         else:
             self.name = self.attr['name']
